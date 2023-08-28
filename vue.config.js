1f1ffbf450f943494fdbdc8171c938e3ca5564a8
Bravo2333 <2087369435@qq.com>
2023-08-28 22:56:18 +0800

    total envirment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const path = require('path');
+
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+  }
+}
+)
